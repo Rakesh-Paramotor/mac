6547a41f2b2584587def0dee52ca82fd822d661f
rakesh <rakesh@paramotordt.in>
2026-01-09 15:03:21 +0530

    adding a  icon on  website  also  adding a  window.jsx which used to  open  close the  application

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,7 +3,11 @@ import {dockApps} from "#constants/index.js";
 import {Tooltip} from "react-tooltip";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+import useWindowStore from "#store/window.jsx";
+
+
 const Dock = () => {
+    const {openWindow, closeWindow, windows} = useWindowStore()
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -48,7 +52,18 @@ const Dock = () => {
     }, []);
 
     const toggleApp = (app) =>{
+            if(!app.canOpen) return;
 
+            const appWindow = windows[app.id];
+
+            if(!appWindow){
+                return;
+            }
+            if(appWindow.isOpen){
+                closeWindow(app.id);
+            }else{
+                openWindow(app.id);
+            }
     }
     return <section id="dock">
         <div ref={dockRef} className="dock-container">
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-import Navbar from "#components/Navbar.jsx";
-import Welcome from "#components/Welcome.jsx";
-import Dock from "#components/Dock.jsx";
+import Navbar from "./Navbar.jsx";
+import Welcome from "./Welcome.jsx";
+import Dock from "./Dock.jsx";
 
 export {
     Navbar, Welcome, Dock
