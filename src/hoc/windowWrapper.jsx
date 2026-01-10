0d5271c3917518fa2dd1d77490ac61316cd3fa91
rakesh <rakesh@paramotordt.in>
2026-01-10 11:48:34 +0530

    adding a animation in windowWrapper.jsx

--- src/hoc/windowWrapper.jsx
+++ src/hoc/windowWrapper.jsx
@@ -1,6 +1,7 @@
-import React, {useLayoutEffect, useRef} from 'react'
+import React, {useRef} from 'react'
 import useWindowStore from "#store/window.jsx";
 import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const WindowWrapper = (Component, windowKey) => {
     const Wrapped = (props) => {
@@ -8,23 +9,35 @@ const WindowWrapper = (Component, windowKey) => {
         const {isOpen, zIndex} = windows[windowKey];
         const ref = useRef(null);
 
-        useGSAP(() =>{
+        useGSAP(() => {
             const el = ref.current;
-            if(!el || !isOpen) return;
+            if (!el) return;
 
-            el.style.display = "block";
-        }, [isOpen])
-
-        useLayoutEffect(() =>{
-            const el = ref.current;
-            if(!el ) return;
-            el.style.display = isOpen ? "block" : "none"
-        }, [isOpen])
+            if (isOpen) {
+                el.style.display = 'block';
+                gsap.fromTo(
+                    el,
+                    {scale: 0.8, opacity: 0, y: 40},
+                    {scale: 1, opacity: 1, y: 0, duration: 0.2, ease: "power1.out"}
+                );
+            } else {
+                gsap.to(el, {
+                    scale: 0.8,
+                    opacity: 0,
+                    y: 40,
+                    duration: 0.2,
+                    ease: "power1.in",
+                    onComplete: () => {
+                        el.style.display = 'none';
+                    }
+                });
+            }
+        }, {scope: ref, dependencies: [isOpen]});
 
         return (
-            <section id={windowKey} ref={ref} style={{zIndex}} className="absolute">
-            <Component {...props}/>
-        </section>
+            <section id={windowKey} ref={ref} style={{zIndex, display: 'none'}} className="absolute">
+                <Component {...props}/>
+            </section>
         );
     };
     Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"})`
